a681d657f6641fb5449b7a3ec05c11a3760d5fe9
247722 Luzak Mateusz <247722@edu.p.lodz.pl>
2024-04-02 17:46:37 +0200

    dodanie klasy ball

---
+++ Dane/Ball.cs
@@ -0,0 +1,10 @@
+﻿namespace Dane
+{
+    public class Ball
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double VelocityX { get; set; }
+        public double VelocityY { get; set; }
+    }
+}
